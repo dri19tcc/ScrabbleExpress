1de9c6c3c469a8a42908cf8fedeacdc90a74584f
Adriana <dri19tcc@gmail.com>
2016-06-14 13:07:31 -0700

    More on learning database.  Added npm tasks to drop, create, and add a schema to the db

--- tasks/load_schema.js
+++ tasks/load_schema.js
@@ -1,9 +1,15 @@
 var massive = require('massive');
-var connectionString = "postgres://localhost";
+var connectionString = "postgres://localhost/scrabble_express";
 
 var db = massive.connectSync({connectionString : connectionString});
 
-db.setup.schema();
+db.setup.schema([], function(err, res) {
+  if(err) {
+    throw(new Error(err.message));
+  }
+  console.log("yay schema!");
+  process.exit();
+});
 
 // run the setup command
 // db.run("CREATE DATABASE scrabble_express;", function(err, res) {
