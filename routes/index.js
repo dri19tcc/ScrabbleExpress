f0e425ad75619936e5d805536cc7487f66bb62a4
Adriana <dri19tcc@gmail.com>
2016-06-13 20:35:15 -0700

    Added a new route so it would go to scrabble/chart.  Made a chart and looped through it so I could show values in a table

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var Controller = require('../controllers/scrabble')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Scrabble Scorer', blurb: ", for all your Scrabble scoring needs" });
-
 });
+
+router.get('/scrabble/chart', Controller.getChart);
+
 
 
 module.exports = router;
